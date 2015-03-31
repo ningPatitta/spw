c53b4fcf83fb337ea9b0ee7318f4a7e60f2da655
PatittaR <ningz_2737@hotmail.com>
2015-04-01 02:27:15 +0700

    *01*

--- f2/spw/Enemy.java
+++ f2/spw/Enemy.java
@@ -3,29 +3,44 @@ package f2.spw;
 import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Enemy extends Sprite{
 	public static final int Y_TO_FADE = 400;
 	public static final int Y_TO_DIE = 600;
 	
-	private int step = 12;
+	private String picEnemy;
+	private Image picture;
+	
+	private int step = 10;
 	private boolean alive = true;
 	
 	public Enemy(int x, int y) {
-		super(x, y, 5, 10);
-		
+		super(x, y, 15, 20);
+		try{
+			//picEnemy = "...//img/enemy.PNG";
+			picEnemy = "E:/My Couse/242-210 fundamental programmingII/spw/f2/spw/img/enemy.png";
+			picture = ImageIO.read(new File(picEnemy));
+		}catch(IOException e){
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void draw(Graphics2D g) {
-		if(y < Y_TO_FADE)
-			g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f));
-		else{
-			g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 
-					(float)(Y_TO_DIE - y)/(Y_TO_DIE - Y_TO_FADE)));
-		}
-		g.setColor(Color.RED);
-		g.fillRect(x, y, width, height);
+		g.drawImage(picture,x,y,width,height,null);
+		//if(y < Y_TO_FADE)
+		//	g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f));
+		//else{
+		//	g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 
+		//			(float)(Y_TO_DIE - y)/(Y_TO_DIE - Y_TO_FADE)));
+		//}
+		//g.setColor(Color.RED);
+		//g.fillRect(x, y, width, height);
 		
 	}
 
